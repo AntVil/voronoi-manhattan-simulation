df13f135055f8b7df207229728bd00b5406b4097
Anton Böhler <antboehler@gmail.com>
2024-06-07 21:01:51 +0200

    finally works..

--- script.js
+++ script.js
@@ -31,12 +31,12 @@ window.onload = () => {
 }
 
 function loop() {
-    if (frame % 10 === 0) {
+    if (frame % 5 === 0) {
         grid.update();
 
         grid.render(ctxt);
 
-        if(step === 300) {
+        if (step === 300) {
             return;
         }
         step++
@@ -56,24 +56,10 @@ class Grid {
     constructor(size) {
         this.size = size;
         this.grid = [];
-        for (let i = 0; i < size+1; i++) {
+        for (let i = 0; i < size + 1; i++) {
             let row = [];
-            for (let j = 0; j < size+1; j++) {
-                let state = 0b0000;
-                if(i > 0) {
-                    state |= 0b1000;
-                }
-                if(j > 0) {
-                    state |= 0b0001;
-                }
-                if(i < size) {
-                    state |= 0b0010;
-                }
-                if(j < size) {
-                    state |= 0b0100;
-                }
-
-                row.push(state);
+            for (let j = 0; j < size + 1; j++) {
+                row.push(new GridCell());
             }
             this.grid.push(row);
         }
@@ -81,10 +67,119 @@ class Grid {
         this.regions = [];
     }
 
+    setCell(x, y) {
+        let hue;
+        do {
+            hue = Math.floor(360 * Math.random());
+        } while (this.regions.includes(hue));
+
+        this.regions.push(hue);
+        this.grid[y][x].addRegion(hue);
+        this.grid[y - 1][x].addRegion(hue);
+        this.grid[y][x + 1].addRegion(hue);
+        this.grid[y + 1][x].addRegion(hue);
+        this.grid[y][x - 1].addRegion(hue);
+    }
+
+    update() {
+        for (let i = 0; i < this.grid.length; i++) {
+            for (let j = 0; j < this.grid[i].length; j++) {
+                this.grid[i][j].update(
+                    this.grid[i - 1]?.[j],
+                    this.grid[i]?.[j + 1],
+                    this.grid[i + 1]?.[j],
+                    this.grid[i]?.[j - 1]
+                );
+            }
+        }
+
+        for (let i = 0; i < this.grid.length; i++) {
+            for (let j = 0; j < this.grid[i].length; j++) {
+                this.grid[i][j].upgrade();
+            }
+        }
+    }
+
+    computePolygonStart(region) {
+        for (let i = 0; i < this.grid.length; i++) {
+            for (let j = 0; j < this.grid[i].length; j++) {
+                if (this.grid[i][j].hasRegion(region)) {
+                    return [i, j]
+                }
+            }
+        }
+    }
+
+    computeRegions() {
+        return this.regions.map((region) => {
+            let [y, x] = this.computePolygonStart(region);
+            let polygon = [[y, x]];
+
+            // move along top-right side
+            while (true) {
+                if (this.grid[y]?.[x + 1]?.hasRegion(region) ?? false) {
+                    x++;
+                } else if (this.grid[y + 1]?.[x + 1]?.hasRegion(region) ?? false) {
+                    x++;
+                    y++;
+                } else if (this.grid[y + 1]?.[x]?.hasRegion(region) ?? false) {
+                    y++;
+                } else {
+                    break;
+                }
+                polygon.push([y, x]);
+            }
+            // move along bottom-right side
+            while (true) {
+                if (this.grid[y + 1]?.[x]?.hasRegion(region) ?? false) {
+                    y++;
+                } else if (this.grid[y + 1]?.[x - 1]?.hasRegion(region) ?? false) {
+                    x--;
+                    y++;
+                } else if (this.grid[y]?.[x - 1]?.hasRegion(region) ?? false) {
+                    x--;
+                } else {
+                    break;
+                }
+                polygon.push([y, x]);
+            }
+            // move along bottom-left side
+            while (true) {
+                if (this.grid[y]?.[x - 1]?.hasRegion(region) ?? false) {
+                    x--;
+                } else if (this.grid[y - 1]?.[x - 1]?.hasRegion(region) ?? false) {
+                    x--;
+                    y--;
+                } else if (this.grid[y - 1]?.[x]?.hasRegion(region) ?? false) {
+                    y--;
+                } else {
+                    break;
+                }
+                polygon.push([y, x]);
+            }
+            // move along top-left side
+            while (y !== polygon[0][0] || x !== polygon[0][1]) {
+                if (this.grid[y - 1]?.[x]?.hasRegion(region) ?? false) {
+                    y--;
+                } else if (this.grid[y - 1]?.[x + 1]?.hasRegion(region) ?? false) {
+                    x++;
+                    y--;
+                } else if (this.grid[y]?.[x + 1]?.hasRegion(region) ?? false) {
+                    x++;
+                } else {
+                    break;
+                }
+                polygon.push([y, x]);
+            }
+
+            return [region, polygon];
+        });
+    }
+
     render(ctxt) {
         ctxt.clearRect(0, 0, canvas.width, canvas.height);
         ctxt.lineWidth = 1;
-
+        ctxt.strokeStyle = "#000";
         ctxt.beginPath();
         for (let i = 0; i < this.grid.length + 1; i += 2) {
             for (let j = 0; j < this.grid.length + 1; j += 2) {
@@ -96,205 +191,76 @@ class Grid {
         }
         ctxt.stroke();
 
-        for (let region of this.regions) {
-            region.render(ctxt);
+        let regions = this.computeRegions();
+        for (let [hue, polygon] of regions) {
+            ctxt.globalAlpha = 0.2;
+            ctxt.beginPath();
+            ctxt.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+            ctxt.fillStyle = `hsl(${hue}, 100%, 50%)`;
+            ctxt.moveTo(CELL_SIZE * polygon[0][1], CELL_SIZE * polygon[0][0]);
+            for (let i = 1; i < polygon.length; i++) {
+                ctxt.lineTo(CELL_SIZE * polygon[i][1], CELL_SIZE * polygon[i][0]);
+            }
+            ctxt.closePath();
+            ctxt.fill();
+            ctxt.globalAlpha = 1;
+            ctxt.stroke();
         }
-    }
-
-    setCell(x, y) {
-        let hue = Math.floor(360 * Math.random());
-
-        this.regions.push(new Region(x, y, hue));
-        this.grid[y][x] &= 0b0000;
-        this.grid[y-1][x] &= 0b1101;
-        this.grid[y][x+1] &= 0b1110;
-        this.grid[y+1][x] &= 0b0111;
-        this.grid[y][x-1] &= 0b1011;
-    }
-
-    update() {
-        let nextGrid = this.grid.map(row => row.slice());
-        let edgeCaseGrid = this.grid.map(row => row.map(() => 0b0));
-        for(let region of this.regions) {
-            region.computeEdgeCase(this.grid, edgeCaseGrid);
-        }
-        for(let region of this.regions) {
-            region.update(this.grid, nextGrid, edgeCaseGrid);
-        }
-        this.grid = nextGrid;
     }
 }
 
-class Region {
-    constructor(x, y, hue) {
-        this.points = [
-            new RegionPoint(x, y - 1),
-            new RegionPoint(x + 1, y),
-            new RegionPoint(x, y + 1),
-            new RegionPoint(x - 1, y)
-        ];
-
-        this.hue = hue;
+class GridCell {
+    constructor() {
+        this.closestDistance = Infinity;
+        this.closestRegions = [];
+        this.nextClosestDistance = Infinity;
+        this.nextClosestRegions = [];
     }
 
-    render(ctxt) {
-        ctxt.fillStyle = `hsl(${this.hue}, 100%, 50%)`;
-        ctxt.lineWidth = 3;
-
-        ctxt.beginPath();
-        ctxt.moveTo(this.points[0].x * CELL_SIZE, this.points[0].y * CELL_SIZE);
-        for (let i = 1; i < this.points.length; i++) {
-            ctxt.lineTo(this.points[i].x * CELL_SIZE, this.points[i].y * CELL_SIZE);
-        }
-        ctxt.closePath();
-        // ctxt.fill()
-        ctxt.stroke()
-
-        for(let point of this.points) {
-            ctxt.beginPath();
-            ctxt.arc(point.x * CELL_SIZE, point.y * CELL_SIZE, 5, 0, 2 * Math.PI);
-            ctxt.fill();
-        }
+    addRegion(region) {
+        this.closestDistance = 0;
+        this.nextClosestDistance = 0;
+        this.closestRegions.push(region);
+        this.nextClosestRegions.push(region);
     }
 
-    computeEdgeCase(grid1, grid2) {
-        for(let point of this.points) {
-            point.computeEdgeCase(grid1, grid2);
-        }
-        for(let i=0;i<grid2.length;i++) {
-            for(let j=0;j<grid2[i].length;j++) {
-                // console.log("abc!", grid2[i][j])
-                if(grid2[i][j] === 0b1) {
-                    grid2[i][j] = grid2[i][j] << 1;
+    isVisited() {
+        return this.closestRegions.length > 0;
+    }
 
-                    // console.log("a", grid2[i][j])
+    hasRegion(region) {
+        return this.closestRegions.includes(region);
+    }
+
+    update(top, right, bottom, left) {
+        this.updateCell(top);
+        this.updateCell(right);
+        this.updateCell(bottom);
+        this.updateCell(left);
+    }
+
+    updateCell(otherCell) {
+        if (otherCell?.isVisited() ?? false) {
+            let candidateDistance = otherCell.closestDistance + 1;
+            if (candidateDistance < this.nextClosestDistance) {
+                this.nextClosestDistance = candidateDistance;
+                this.nextClosestRegions = otherCell.closestRegions.slice();
+            }
+            else if (candidateDistance === this.nextClosestDistance) {
+                for (let region of otherCell.closestRegions) {
+                    if (this.nextClosestRegions.includes(region)) {
+                        continue;
+                    }
+
+                    this.nextClosestRegions.push(region);
                 }
             }
         }
     }
 
-    update(grid1, grid2, grid3) {
-        this.points = this.points.flatMap(p => p.generateNext(grid1, grid2, grid3));
-        for(let i=this.points.length-1;i>=0;i--) {
-            let j = (i - 1 + this.points.length) % this.points.length;
-            if(this.points[i].equalTo(this.points[j])) {
-                this.points.splice(i, 1);
-            }
-        }
-    }
-}
-
-class RegionPoint {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    equalTo(other) {
-        return (this.x === other.x && this.y === other.y);
-    }
-
-    computeEdgeCase(grid1, grid2) {
-        let state = grid1[this.y][this.x];
-
-        // if((state & 0b1000) === 0b1000) {
-        //     grid2[this.y-1][this.x] |= 0b1;
-        // }
-        // if((state & 0b0100) === 0b0100) {
-        //     grid2[this.y][this.x+1] |= 0b1;
-        // }
-        // if((state & 0b0010) === 0b0010) {
-        //     grid2[this.y+1][this.x] |= 0b1;
-        // }
-        // if((state & 0b0001) === 0b0001) {
-        //     grid2[this.y][this.x-1] |= 0b1;
-        // }
-
-        grid2[this.y][this.x] |= 0b1;
-    }
-
-    generateNext(grid1, grid2, grid3) {
-        let state = grid1[this.y][this.x];
-        grid2[this.y][this.x] &= 0b0000;
-
-        switch (state) {
-        case 0b0000:
-            return [this];
-        case 0b0001:
-            grid2[this.y][this.x-1] &= 0b1011;
-            return [new RegionPoint(this.x-1, this.y), this];
-        case 0b0010:
-            grid2[this.y+1][this.x] &= 0b0111;
-            return [new RegionPoint(this.x, this.y+1), this];
-        case 0b0011:
-            grid2[this.y+1][this.x] &= 0b0111;
-            grid2[this.y][this.x-1] &= 0b1011;
-            if(grid3[this.y][this.x] === 0b11) {
-                return [new RegionPoint(this.x, this.y+1), this, new RegionPoint(this.x-1, this.y)];
-            } else {
-                return [new RegionPoint(this.x, this.y+1), new RegionPoint(this.x-1, this.y)];
-            }
-        case 0b0100:
-            grid2[this.y][this.x+1] &= 0b1110;
-            return [new RegionPoint(this.x+1, this.y), this];
-        case 0b0101:
-            grid2[this.y][this.x+1] &= 0b1110;
-            grid2[this.y][this.x-1] &= 0b1011;
-            return [new RegionPoint(this.x+1, this.y), this, new RegionPoint(this.x-1, this.y)];
-        case 0b0110:
-            grid2[this.y][this.x+1] &= 0b1110;
-            grid2[this.y+1][this.x] &= 0b0111;
-            if(grid3[this.y][this.x] === 0b11) {
-                return [new RegionPoint(this.x+1, this.y), this, new RegionPoint(this.x, this.y+1)];
-            }else {
-                return [new RegionPoint(this.x+1, this.y), new RegionPoint(this.x, this.y+1)];
-            }
-        case 0b0111:
-            grid2[this.y][this.x+1] &= 0b1110;
-            grid2[this.y+1][this.x] &= 0b0111;
-            grid2[this.y][this.x-1] &= 0b1011;
-            return [new RegionPoint(this.x+1, this.y), new RegionPoint(this.x, this.y+1), new RegionPoint(this.x-1, this.y)];
-        case 0b1000:
-            grid2[this.y-1][this.x] &= 0b1101;
-            return [new RegionPoint(this.x, this.y-1), this];
-        case 0b1001:
-            grid2[this.y][this.x-1] &= 0b1011;
-            grid2[this.y-1][this.x] &= 0b1101;
-            if(grid3[this.y][this.x] === 0b11) {
-                return [new RegionPoint(this.x-1, this.y), this, new RegionPoint(this.x, this.y-1)];
-            }else {
-                return [new RegionPoint(this.x-1, this.y), new RegionPoint(this.x, this.y-1)];
-            }
-        case 0b1010:
-            grid2[this.y+1][this.x] &= 0b0111;
-            grid2[this.y-1][this.x] &= 0b1101;
-            return [new RegionPoint(this.x, this.y+1), this, new RegionPoint(this.x, this.y-1)];
-        case 0b1011:
-            grid2[this.y+1][this.x] &= 0b0111;
-            grid2[this.y][this.x-1] &= 0b1011;
-            grid2[this.y-1][this.x] &= 0b1101;
-            return [new RegionPoint(this.x, this.y+1), new RegionPoint(this.x-1, this.y), new RegionPoint(this.x, this.y-1)];
-        case 0b1100:
-            grid2[this.y-1][this.x] &= 0b1101;
-            grid2[this.y][this.x+1] &= 0b1110;
-            if(grid3[this.y][this.x] === 0b11) {
-                return [new RegionPoint(this.x, this.y-1), this, new RegionPoint(this.x+1, this.y)];
-            }else {
-                return [new RegionPoint(this.x, this.y-1), new RegionPoint(this.x+1, this.y)];
-            }
-        case 0b1101:
-            grid2[this.y][this.x-1] &= 0b1011;
-            grid2[this.y-1][this.x] &= 0b1101;
-            grid2[this.y][this.x+1] &= 0b1110;
-            return [new RegionPoint(this.x-1, this.y), new RegionPoint(this.x, this.y-1), new RegionPoint(this.x+1, this.y)];
-        case 0b1110:
-            grid2[this.y-1][this.x] &= 0b1101;
-            grid2[this.y][this.x+1] &= 0b1110;
-            grid2[this.y+1][this.x] &= 0b0111;
-            return [new RegionPoint(this.x, this.y-1), new RegionPoint(this.x+1, this.y), new RegionPoint(this.x, this.y+1)];
-
-        case 0b1111:
-            return [new RegionPoint(this.x, this.y-1), new RegionPoint(this.x+1, this.y), new RegionPoint(this.x, this.y+1), new RegionPoint(this.x-1, this.y)];
-        }
+    upgrade() {
+        this.closestDistance = this.nextClosestDistance;
+        this.closestRegions = this.nextClosestRegions.slice();
+        // console.log(this.nextClosestRegions)
     }
 }
